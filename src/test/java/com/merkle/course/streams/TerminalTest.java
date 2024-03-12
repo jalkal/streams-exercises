f7b5758ac90e94f8e576af5f87faec47e1dd6886
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 15:07:40 +0100

    add solutions

--- src/test/java/com/merkle/course/streams/TerminalTest.java
+++ src/test/java/com/merkle/course/streams/TerminalTest.java
@@ -2,6 +2,8 @@ package com.merkle.course.streams;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -9,19 +11,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TerminalTest {
 
+    Stream<Employee> employeeStream = Stream.of(new Employee("Steve", 185.00), new Employee("James", 200.00), new Employee("Craig", 170.00));
+
     /**
-     * use Stream forEach to consume an Stream
+     * use Stream forEach to consume a Stream
      */
     @Test
-    void stream_forEach(){
-        class ConsumerTest implements Consumer<Integer>{
+    void stream_forEach() {
+        class ConsumerTest implements Consumer<Integer> {
             Integer accumulator = 0;
+
             @Override
             public void accept(Integer value) {
                 accumulator = accumulator + value;
             }
 
-            Integer getAccumulator(){
+            Integer getAccumulator() {
                 return this.accumulator;
             }
         }
@@ -30,5 +35,68 @@ class TerminalTest {
         stream.forEach(consumerTest);
 
         assertThat(consumerTest).extracting(ConsumerTest::getAccumulator).isEqualTo(15);
+    }
+
+    /**
+     * Get first employee from Stream
+     */
+    @Test
+    void stream_findFirst() {
+
+        Optional<Employee> optionalEmployee = employeeStream.findFirst();
+        assertThat(optionalEmployee).hasValueSatisfying(employee -> assertThat(employee).extracting("name").isEqualTo("Steve"));
+    }
+
+    /**
+     * Get employee with the lowest salary
+     */
+    @Test
+    void stream_min() {
+
+        Optional<Employee> optionalEmployee = employeeStream.min(Comparator.comparing(Employee::salary));
+        assertThat(optionalEmployee).hasValueSatisfying(employee -> assertThat(employee).extracting("name").isEqualTo("Craig"));
+    }
+
+    /**
+     * Get employee with the highest salary
+     */
+    @Test
+    void stream_max() {
+
+        Optional<Employee> optionalEmployee = employeeStream.max(Comparator.comparing(Employee::salary));
+        assertThat(optionalEmployee).hasValueSatisfying(employee -> assertThat(employee).extracting("name").isEqualTo("James"));
+    }
+
+    /**
+     * Get sum all employee salaries
+     */
+    @Test
+    void stream_sum() {
+
+        Double totalSalaries = employeeStream.mapToDouble(Employee::salary).sum();
+        assertThat(totalSalaries).isEqualTo(555.00);
+    }
+
+    /**
+     * Get average of all employee salaries
+     */
+    @Test
+    void stream_average() {
+
+        Double averageSalaries = employeeStream.mapToDouble(Employee::salary).average().orElseThrow();
+        assertThat(averageSalaries).isEqualTo(185.00);
+    }
+
+    /**
+     * Find if any employee has a salary higher than 200.00
+     */
+    @Test
+    void stream_anyMatch(){
+
+        boolean anySalaryHigherThan200 = employeeStream.mapToDouble(Employee::salary).anyMatch(salary -> salary > 200.00);
+        assertThat(anySalaryHigherThan200).isFalse();
+    }
+
+    record Employee(String name, Double salary) {
     }
 }
