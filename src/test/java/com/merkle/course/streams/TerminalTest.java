edb21933abdf56d3578fe3934044a3cbcf28caa1
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 12:58:53 +0100

    add stream_forEach

---
+++ src/test/java/com/merkle/course/streams/TerminalTest.java
@@ -0,0 +1,34 @@
+package com.merkle.course.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TerminalTest {
+
+    /**
+     * use Stream forEach to consume an Stream
+     */
+    @Test
+    void stream_forEach(){
+        class ConsumerTest implements Consumer<Integer> {
+            Integer accumulator = 0;
+            @Override
+            public void accept(Integer value) {
+                accumulator = accumulator + value;
+            }
+
+            Integer getAccumulator(){
+                return this.accumulator;
+            }
+        }
+        ConsumerTest consumerTest = new ConsumerTest();
+        Stream<Integer> stream = Stream.of(5, 4, 3, 2, 1);
+        //Implement here what's required
+
+        assertThat(consumerTest).extracting(ConsumerTest::getAccumulator).isEqualTo(15);
+    }
+}
