8afb9b3667c84d8c087a483cc4d127698b804ae7
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 10:24:33 +0100

    add stream_rangeClosed

--- src/test/java/com/merkle/course/streams/SourcesTest.java
+++ src/test/java/com/merkle/course/streams/SourcesTest.java
@@ -3,6 +3,7 @@ package com.merkle.course.streams;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,6 +47,16 @@ class SourcesTest {
      */
     @Test
     void stream_range(){
+        Stream<Integer> stream = null; //Modify this line
+
+        assertThat(stream).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    /**
+     * Create Stream using IntStream.rangeClosed()
+     */
+    @Test
+    void stream_rangeClosed(){
         Stream<Integer> stream = null; //Modify this line
 
         assertThat(stream).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
