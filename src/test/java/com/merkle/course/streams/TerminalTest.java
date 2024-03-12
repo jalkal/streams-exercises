dff6f1019635ec2e55b650e71ab811e7c56e82c6
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 12:57:31 +0100

    solution stream_forEach

--- src/test/java/com/merkle/course/streams/TerminalTest.java
+++ src/test/java/com/merkle/course/streams/TerminalTest.java
@@ -14,22 +14,21 @@ class TerminalTest {
      */
     @Test
     void stream_forEach(){
+        class ConsumerTest implements Consumer<Integer>{
+            Integer accumulator = 0;
+            @Override
+            public void accept(Integer value) {
+                accumulator = accumulator + value;
+            }
+
+            Integer getAccumulator(){
+                return this.accumulator;
+            }
+        }
         ConsumerTest consumerTest = new ConsumerTest();
         Stream<Integer> stream = Stream.of(5, 4, 3, 2, 1);
         stream.forEach(consumerTest);
 
         assertThat(consumerTest).extracting(ConsumerTest::getAccumulator).isEqualTo(15);
-    }
-
-    static class ConsumerTest implements Consumer<Integer>{
-        Integer accumulator = 0;
-        @Override
-        public void accept(Integer value) {
-            accumulator = accumulator + value;
-        }
-
-        Integer getAccumulator(){
-            return this.accumulator;
-        }
     }
 }
