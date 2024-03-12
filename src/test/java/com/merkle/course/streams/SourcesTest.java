055f2e5ed4a921881b6173a73e9a02c5a32f1a96
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 10:14:13 +0100

    solution stream_iterate

--- src/test/java/com/merkle/course/streams/SourcesTest.java
+++ src/test/java/com/merkle/course/streams/SourcesTest.java
@@ -1,10 +1,11 @@
 package com.merkle.course.streams;
 
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SourcesTest {
 
@@ -15,7 +16,7 @@ class SourcesTest {
     void stream_of(){
         Stream<String> stream = Stream.of("hello", "world");
 
-        Assertions.assertThat(stream).containsExactly("hello", "world");
+        assertThat(stream).containsExactly("hello", "world");
     }
 
     /**
@@ -23,10 +24,20 @@ class SourcesTest {
      */
     @Test
     void stream_generate(){
-        Condition<String> isHello = new Condition<>(s -> s.equals("hello"), "Are hello");
+        Condition<String> isHello = new Condition<>(s -> s.equals("hello"), "is hello");
 
-        Stream<String> stream = Stream.generate(()-> "hello").limit(10); //Modify this line
+        Stream<String> stream = Stream.generate(()-> "hello").limit(10);
 
-        Assertions.assertThat(stream).areExactly(10, isHello);
+        assertThat(stream).areExactly(10, isHello);
+    }
+
+    /**
+     * Create Stream using Stream.iterate()
+     */
+    @Test
+    void stream_iterate(){
+        Stream<Integer> stream = Stream.iterate(0, i-> i <= 10,  i-> i + 1);
+
+        assertThat(stream).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 }
