6bac270b3b3bb0715382149ab9c3af209f7184bd
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 11:00:55 +0100

    add stream_filter

--- src/test/java/com/merkle/course/streams/IntermediateTest.java
+++ src/test/java/com/merkle/course/streams/IntermediateTest.java
@@ -1,9 +1,10 @@
 package com.merkle.course.streams;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class IntermediateTest {
 
@@ -12,8 +13,19 @@ class IntermediateTest {
      */
     @Test
     void stream_map(){
-        Stream<String> stream = Stream.of(1, 2, 3, 4);
+        //Todo uncomment this line
+        Stream<String> stream = null; //Stream.of(1, 2, 3, 4); //Modify this line
 
-        Assertions.assertThat(stream).containsExactly("1", "2", "3", "4");
+        assertThat(stream).containsExactly("1", "2", "3", "4");
+    }
+
+    /**
+     * Filter Stream items with filter()
+     */
+    @Test
+    void stream_filter(){
+        Stream<String> stream = Stream.of("hello", "", "world");
+
+        assertThat(stream).containsExactly("hello", "world");
     }
 }
