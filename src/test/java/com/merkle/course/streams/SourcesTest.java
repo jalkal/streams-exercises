5003bc2407d93b55add5a83312b0237b281604fd
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 09:58:17 +0100

    add stream_generate

--- src/test/java/com/merkle/course/streams/SourcesTest.java
+++ src/test/java/com/merkle/course/streams/SourcesTest.java
@@ -1,6 +1,7 @@
 package com.merkle.course.streams;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
@@ -8,12 +9,24 @@ import java.util.stream.Stream;
 class SourcesTest {
 
     /**
-     * Generate list using Stream.of()
+     * Create Stream using Stream.of()
      */
     @Test
     void stream_of(){
         Stream<String> stream = null; //Modify this line
 
         Assertions.assertThat(stream.toList()).containsExactly("hello", "world");
+    }
+
+    /**
+     * Create Stream using Stream.generate()
+     */
+    @Test
+    void stream_generate(){
+        Condition<String> isHello = new Condition<>(s -> s.equals("hello"), "Are hello");
+
+        Stream<String> stream = null; //Modify this line
+
+        Assertions.assertThat(stream).areExactly(10, isHello);
     }
 }
