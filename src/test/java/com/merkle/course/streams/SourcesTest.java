aa7380b7eb0a99ee244becf4816e2be650b26ef3
Jose Alcalde <jose.alcalde@sky.uk>
2024-03-12 10:14:53 +0100

    add stream_iterate

--- src/test/java/com/merkle/course/streams/SourcesTest.java
+++ src/test/java/com/merkle/course/streams/SourcesTest.java
@@ -38,6 +38,6 @@ class SourcesTest {
     void stream_iterate(){
         Stream<Integer> stream = null; //Modify this line
 
-        assertThat(stream).containsExactly(0, 1, 2, 4, 5, 6, 7, 8, 9, 10);
+        assertThat(stream).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 }
